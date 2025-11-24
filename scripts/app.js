5a82c85588d274090ab1f46669244ecbc120e3a9
dherrera-code <dherr929@gmail.com>
2025-11-24 11:30:14 -0800

    created id for student elements and button

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+//create data elements to manipulate DOM
+
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let codestackEmail = document.getElementById("codeStackEmail");
+let email = document.getElementById("email");
+let rngBtn = document.getElementById("randomBtn");
+
